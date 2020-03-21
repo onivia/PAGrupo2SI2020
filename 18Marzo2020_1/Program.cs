cffaba8deff1566e69e6a3ba78ed486f2d0a31e6
Oscar Hernando Nivia Aragón <ONivia@agoracsc.com>
2020-03-20 19:50:31 -0500

    ... polimorfismo

--- 18Marzo2020_1/Program.cs
+++ 18Marzo2020_1/Program.cs
@@ -8,33 +8,44 @@ namespace _18Marzo2020_1
     {
         public static void Main(string[] args)
         {
-            
+            probarDocElectronico();
         }
 
         public static void probarDocElectronico() {
             DocumentoElectronico docElec = null;
             
-            docElec = DocumentoElectronico.crearDocumentoElectronico(3);
-            docElec.calcularTotal();
+            docElec = DocumentoElectronico.crearDocumentoElectronico(2);
+            //docElec.calcularTotal();
+            anaularDocumento(docElec);
 
             IOperacionDocSoporte doc = null;
-            doc = new OrdenCompra();
-            obtenerFormaPago(doc);
+            doc = new FacturaVentaNacional();
+            //obtenerFormaPago(doc);
 
             //MALA PRACTICA (sin polimorfismo)
             FacturaVentaNacional fvn = null;
             fvn = new FacturaVentaNacional();
-            fvn.calcularTotal();
+            //fvn.calcularTotal();
             //
             NotaCredito nc = null;
             nc = new NotaCredito();
-            nc.calcularTotal();
+            //nc.calcularTotal();
             //
-            //para ND        
+            //para ND
         }
 
         public static void obtenerFormaPago(IOperacionDocSoporte doc) {
             doc.obtenerFormaPago();
+            if(doc.GetType() == typeof(OrdenCompra)) {
+                Console.WriteLine("el objeto doc es de tipo: OrdenCompra");
+            } else if(doc.GetType() == typeof(FacturaVentaNacional)) {
+                Console.WriteLine("el objeto doc es de tipo: FacturaVentaNacional");
+            }
+        }
+    
+        public static void anaularDocumento(DocumentoElectronico docElec) {
+            //los metodos, tiene ser tratamiento COMUN
+            docElec.anularDocumento();
         }
     }
 }
