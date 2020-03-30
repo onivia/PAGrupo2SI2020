2c62a05782cffe89a88444e82d6f357cae479c76
Oscar Hernando Nivia Aragón <ONivia@agoracsc.com>
2020-03-30 18:48:54 -0500

    Ejemplo Inmtabilidad.

---
+++ 30Marzo2020_1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _30Marzo2020_1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Inmutabilidad
+            string s1 = "100";
+            string s2 = s1;
+            s1 = "200";
+            Console.WriteLine($"s1: {s1}");
+            Console.WriteLine($"s2: {s2}");
+        }
+    }
+}
