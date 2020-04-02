ef8a9d0125153069d6e8a0cb7f4d4af0e18dbb5c
Oscar Hernando Nivia Aragón <ONivia@agoracsc.com>
2020-04-01 19:25:05 -0500

    Implementacones compareTo, ToString, Clone.

--- 30Marzo2020_1/Program.cs
+++ 30Marzo2020_1/Program.cs
@@ -1,8 +1,24 @@
-﻿using System;
+﻿using System.Linq;
+using System;
 
 namespace _30Marzo2020_1
 {
-    public class A { //mutable
+    public class A : IComparable, ICloneable { //mutable
+
+        public A(){ }
+
+        public A(A a)
+        {
+            this.i1 = a.i1;
+            this.s1 = a.s1;            
+        }
+
+        public A(int i1, string s1)
+        {
+            this.i1 = i1;
+            this.s1 = s1;
+        }
+
         public A(string s1)
         {            
             this.s1 = s1;
@@ -29,6 +45,36 @@ namespace _30Marzo2020_1
 
             return sonIguales;
         }
+
+        public override string ToString() {
+            return $"i1: {this.i1}, s1: {this.s1}"; 
+        }
+
+        public int CompareTo(object obj)
+        {
+            A objComparado = null;
+            if(obj.GetType() == typeof(A)) {
+                objComparado = (A)obj;
+                
+                if(this.i1 < objComparado.i1)
+                    return -1; //this es menor, por i1
+                else if(this.i1 > objComparado.i1)
+                    return 1; //this es mayor, por i1
+                else {
+                    return 0; //ambos objetos son iguales
+                }
+            }
+            throw new Exception("el objeto obj NO es de tipo 'A'");
+        }
+
+        public object Clone()
+        {
+            // A nuevoObjCopia = new A();
+            // nuevoObjCopia.i1 = this.i1;
+            // nuevoObjCopia.s1 = this.s1;
+
+            return new A(this);
+        }
     }
 
     public class AInmutable { //Inmutable
@@ -47,15 +93,29 @@ namespace _30Marzo2020_1
     {
         public static void Main(string[] args)
         {
-            A a1 = new A("100");
-            A a2 = new A("100");
+            A a1 = new A(200, "segundo");
+            A a2 = new A(100,"primero");
+            //A a4 = a2;//Clone
+            A a4 = (A)a2.Clone();
             
+            //Console.WriteLine($"Son Iguales?: {a4.Equals(a2)}");
+            A[] arreglo1 = {a1,a2};
+
+            foreach(A a in arreglo1) {
+                Console.WriteLine(a);                
+            }
+            
+            //Array.Sort(arreglo1);
+            //Array.ForEach(arreglo1,Console.WriteLine);
+            //Console.WriteLine(a1);
+            
+            #region 30Marzo2020
             //Console.WriteLine($"hashcode a1: {a1.GetHashCode()}");
             //Console.WriteLine($"hashcode a2: {a2.GetHashCode()}");
 
-            if(a1.Equals(a2)) {
-                Console.WriteLine("a1 y a2 son iguales");
-            }
+            // if(a1.Equals(a2)) {
+            //     Console.WriteLine("a1 y a2 son iguales");
+            // }
 
             // if(a1 == a2) {
             //     Console.WriteLine("a1 y a2 son iguales");
@@ -70,6 +130,7 @@ namespace _30Marzo2020_1
             a3 = new AInmutable("mundo2");
             a3.asignarValor("mundo2");
             //a3.s1 = "";
+            #endregion
         }
     }
 }
