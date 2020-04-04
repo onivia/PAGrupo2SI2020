63bb1c55b564b2700dcd2f4358ef07ff73f3b50f
Oscar Hernando Nivia Aragón <ONivia@agoracsc.com>
2020-04-03 19:49:32 -0500

    Demostracion EntityFramework.

---
+++ 03Abril2020_1/Modelos/NorthwindContext.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace _03Abril2020_1.Modelos
+{
+    public partial class NorthwindContext : DbContext
+    {
+        public NorthwindContext()
+        {
+        }
+
+        public NorthwindContext(DbContextOptions<NorthwindContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Customers> Customers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Data Source=200.13.195.129,9191;User ID=usuario00;Password=Husuari.00;Initial Catalog=dbpruebas00;Persist Security Info=False;Pooling=False;MultipleActiveResultSets=False;Encrypt=False;TrustServerCertificate=False");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.6-servicing-10079");
+
+            modelBuilder.Entity<Customers>(entity =>
+            {
+                entity.HasKey(e => e.CustomerId);
+
+                entity.HasIndex(e => e.City)
+                    .HasName("City");
+
+                entity.HasIndex(e => e.CompanyName)
+                    .HasName("CompanyName");
+
+                entity.HasIndex(e => e.PostalCode)
+                    .HasName("PostalCode");
+
+                entity.HasIndex(e => e.Region)
+                    .HasName("Region");
+
+                entity.Property(e => e.CustomerId)
+                    .HasColumnName("CustomerID")
+                    .HasMaxLength(5)
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.Address).HasMaxLength(60);
+
+                entity.Property(e => e.City).HasMaxLength(15);
+
+                entity.Property(e => e.CompanyName)
+                    .IsRequired()
+                    .HasMaxLength(40);
+
+                entity.Property(e => e.ContactName).HasMaxLength(30);
+
+                entity.Property(e => e.ContactTitle).HasMaxLength(30);
+
+                entity.Property(e => e.Country).HasMaxLength(15);
+
+                entity.Property(e => e.Fax).HasMaxLength(24);
+
+                entity.Property(e => e.Phone).HasMaxLength(24);
+
+                entity.Property(e => e.PostalCode).HasMaxLength(10);
+
+                entity.Property(e => e.Region).HasMaxLength(15);
+            });
+        }
+    }
+}
---
+++ 03Abril2020_1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using _03Abril2020_1.Modelos;
+
+namespace _03Abril2020_1
+{
+    public class Program
+    {
+        //Prueba CRUD
+        public static void Main(string[] args)
+        {
+            prueba_Delete();            
+        }
+
+        public static void prueba_Delete() {
+            Customers cust = null;
+            using(NorthwindContext contexto = new NorthwindContext()) {
+                cust = contexto.Customers.Where(x => x.CustomerId.Equals("WICKK")).First();
+                contexto.Remove<Customers>(cust);
+                contexto.SaveChanges();
+            }
+        }
+        public static void prueba_Update() {
+            Customers cust = null;
+            using(NorthwindContext contexto = new NorthwindContext()) {
+                cust = contexto.Customers.Where(x => x.CustomerId.Equals("WICKK")).First();
+                cust.City = "Medellin";
+                contexto.Update<Customers>(cust);
+                contexto.SaveChanges();
+            }
+            Console.WriteLine("Se actualizo el registro OK!");
+        }
+        public static void prueba_Insert() {
+            Customers cust = null;
+            cust = new Customers();
+            cust.CustomerId = "WICKK";
+            cust.ContactName = "Oskr Nivia";
+            cust.CompanyName = "Panaderia 123";
+            using(NorthwindContext contexto = new NorthwindContext()) {
+                contexto.Add<Customers>(cust);
+                contexto.SaveChanges();               
+            }
+            Console.WriteLine("se creo registro OK!");
+        }
+        public static void prueba_Select() {
+            Customers cust = null;
+            using(NorthwindContext contexto = new NorthwindContext()) {
+               cust = contexto.Customers.Where(x => x.CustomerId.Equals("BLAUS")).First();
+            }
+            Console.WriteLine($"Ciudad: {cust.City}");
+        }
+    }
+}
