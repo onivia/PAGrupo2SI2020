aac06d77a0256107fac52675e1d7b0f7f6cdb24b
Oscar Hernando Nivia Aragón <ONivia@agoracsc.com>
2020-04-13 20:02:15 -0500

    Uso de Colecciones.

---
+++ 13Abril2020_1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _13Abril2020_1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Prueba_List();
+            //Prueba_Dictionary();
+        }
+
+        //Tips, uso de List:
+        /*
+            multiples elementos
+            no se tiene un identificador unico de cada elemento
+            no importa si hay repetidos
+            no importa el tiempo de respuesta de las busquedas
+        */
+        public static void Prueba_List() {
+            List<string> nombres = new List<string>() {"oskr", "pedrito", "maria"};
+            nombres.Add("juan");
+            nombres.Add("jose");
+            nombres.Add("maria");
+
+            nombres[5] = "manuel";
+            nombres.Remove("manuel");
+
+            foreach(string i in nombres) {
+                Console.WriteLine($"{i}");
+            }
+        }
+
+        //Tips, uso de Dictionary:
+        /*
+            multiples elementos
+            se tiene un identificador unico de cada elemento
+            cuando no hay repetidos por su key
+            se necesitan atender rapidamente busquedas
+        */
+        public static void Prueba_Dictionary() {
+            Dictionary<int,string> nombres = new Dictionary<int, string>(){
+                {100,"oskr"},
+                {200,"pedrito"},
+                {300,"maria"}
+            };
+            nombres.Add(500,"juan");
+            nombres.Add(400,"jose");
+
+            nombres[300] = "luz";
+            nombres.Remove(500);
+
+            foreach(KeyValuePair<int,string> i in nombres) {
+                Console.WriteLine($"Key: {i.Key}-value: {i.Value}");
+            }
+        }
+    }
+}
