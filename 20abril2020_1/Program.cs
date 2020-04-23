b7979a3b15d33c5e3ba0d8c56d48ade4f6e4db55
Oscar Hernando Nivia Aragón <ONivia@agoracsc.com>
2020-04-22 19:51:29 -0500

    Implementacion MiLista

--- 20abril2020_1/Program.cs
+++ 20abril2020_1/Program.cs
@@ -6,7 +6,22 @@ namespace _20abril2020_1
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Prueba_MiLista();            
+        }
+
+        public static void Prueba_MiLista() {
+            MiLista lista = new MiLista();
+            lista.Adicionar(10);
+            lista.Adicionar(20);
+            lista.Adicionar(30);
+
+            lista.InsertarNodoxPosicion(new Nodo(220),0);
+
+            //lista.RemoverPrimero();
+            //Nodo nodo = lista.ObteneryRemoverPrimero();
+            //Console.WriteLine($"Se obtuvo el dato: {nodo.dato}");
+
+            lista.Mostar();
         }
     }
 }
--- 20abril2020_1/base/MiLista.cs
+++ 20abril2020_1/base/MiLista.cs
@@ -50,16 +50,68 @@ namespace _20abril2020_1
             }
         }
 
-        public void RemoverPriemro() {
-
+        public void RemoverPrimero() {
+            //no tiene q estar vacia.
+            //Caso1: la lista tiene un nodo.
+            //Caso2: la lista tiene 2 o mas nodos.
+            Nodo nodoAux = null;
+            
+            if(!estaVacia()) {
+                if(tamano==1) {
+                    p = null;
+                    u = null;                    
+                    tamano = 0;
+                } else {
+                    nodoAux = p;
+                    p = p.sgte;
+                    nodoAux.sgte = null;
+                    nodoAux = null;                    
+                    tamano -= 1; //tamano--;
+                }                
+            }
         }
 
         public Nodo ObteneryRemoverPrimero() {
+            Nodo nodoCopia = null;
 
+            if(!estaVacia()) {
+                nodoCopia = new Nodo(p.dato);
+                RemoverPrimero();
+            }
+
+            return nodoCopia;
         }
 
-        public void InsertarNodoxPOsicion(Nodo nodo, int posi) {
+        public void InsertarNodoxPosicion(Nodo nodo, int posi) {
+            Nodo nodoAux = null;
+            int i = 0;
 
+            if(nodo==null)
+                throw new Exception("nodo NO puede ser null");
+            if((posi<0 || posi > tamano) || (estaVacia() && posi!=0))
+                throw new Exception("posi no es valida");
+            
+            if(estaVacia() && posi==0) { //insertar el nuevo 1° nodo
+                Adicionar(nodo.dato);
+            } else if(!estaVacia()) { //casos para una lista NO vacia
+                if(posi==0) { //insertar el nuevo 1° nodo
+                    nodo.sgte = p;
+                    p = nodo;
+                } else if(posi == tamano) { //insertar nuevo ultimo
+                    u.sgte = nodo;
+                    u = nodo;
+                } else  { //insertar en una posicion intermedia
+                    nodoAux = p;
+                    i = 0;
+                    while(i < (posi-1)) {
+                        nodoAux = nodoAux.sgte; //movemos a nodoAux al siguiente nodo
+                        i++;                        
+                    }
+                    nodo.sgte = nodoAux.sgte;
+                    nodoAux.sgte = nodo;
+                }
+            }
+            tamano += 1; //tamano++
         }
     }
 }
